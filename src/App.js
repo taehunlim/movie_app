9c9dd069644dbc949e3c63e6314fe3a49e3a9817
taehunlim <dkslsdkdl@naver.com>
2019-11-09 11:01:46 +0900

    first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Potato from './potato';
 
 function App() {
   return (
     <div className="App">
-      <h1>아무거나</h1>
+        <h1>아무거나</h1>
+        <Potato/>
     </div>
   );
 }
