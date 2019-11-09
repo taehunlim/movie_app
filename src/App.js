1b73a78bcfb4c94bac99fc8d1ab231797afe41a8
taehunlim <dkslsdkdl@naver.com>
2019-11-09 11:28:40 +0900

    protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -6,31 +6,36 @@ const foodILike = [
         id : 1,
         name: "Kimchi",
         image:
-            "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+            "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+        rating : 4.0
     },
     {
         id : 2,
         name: "Samgyeopsal",
         image:
-            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+            "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+        rating : 4.0
     },
     {
         id : 3,
         name: "Bibimbap",
         image:
-            "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+            "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+        rating : 4.0
     },
     {
         id : 4,
         name: "Doncasu",
         image:
-            "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+            "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+        rating : 4.0
     },
     {
         id : 5,
         name: "Kimbap",
         image:
-            "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+            "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+        rating : 4.0
     }
 ];
 
@@ -43,7 +48,12 @@ function App() {
   return (
     <div className="App">
         {foodILike.map(dish => (
-            <Potato key={dish.id} picture={dish.image} favourite={dish.name}/>
+            <Potato
+                key={dish.id}
+                rating={dish.rating}
+                picture={dish.image}
+                favourite={dish.name}
+            />
         ))}
     </div>
   );
--- src/potato.js
+++ src/potato.js
@@ -1,13 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Potato({favourite, picture}) {
+function Potato({favourite, picture, rating}) {
     return (
         <div>
             <h3>I like {favourite}</h3>
-            <img src={picture} />
+            <img src={picture} alt={favourite}/>
+            <h4>{rating} / 5.0</h4>
         </div>
     );
 }
+
+Potato.propTypes = {
+  favourite : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number
+};
 
 
 export default Potato;
